a1b95458fa9d35021f811059202bb5949f03dbe4
sweathasanthosh <sweathasanthosh798@gmail.com>
2026-04-30 06:34:54 -0700

    added new files and updated files

---
+++ sweatha_phase3/arrays/array.js
@@ -0,0 +1,4 @@
+const items = ["pen","pencil","eraser","sharpener"];
+items.forEach(element => {
+    console.log(element)
+});
---
+++ sweatha_phase3/functions/even_odd.js
@@ -0,0 +1,11 @@
+function even_odd(num){
+    if(num%2===0){
+        return "Even";
+    } else{
+        return "Odd";
+    }
+}
+
+console.log(even_odd(23));
+console.log(even_odd(44));
+console.log(even_odd(28));
